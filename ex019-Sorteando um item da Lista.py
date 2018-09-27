8c68f1fce6089cf3786b38ba07b593b0a650f8b1
guihlr <guilhermehlr9@gmail.com>
2018-09-27 18:35:59 -0300

    initial commmit

---
+++ ex019-Sorteando um item da Lista.py
@@ -0,0 +1,11 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome
+# dos alunos e escrevendo na tela o nome do escolhido.
+
+import random
+n1 = str(input('Primeiro Aluno: '))
+n2 = str(input('Segundo Aluno: '))
+n3 = str(input('Terceiro Aluno: '))
+n4 = str(input('Quatro Aluno: '))
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print('O Aluno escolhido foi {}' .format(escolhido))

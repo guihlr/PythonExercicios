1ef9284666b4dfdd11cc9c04c80cac149127f643
guihlr <guilhermehlr9@gmail.com>
2018-09-27 18:41:51 -0300

    initial commmit

--- ex021-Tocando um MP3.py
+++ ex021-Tocando um MP3.py
@@ -1,7 +1,14 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 import pygame
+
 pygame.mixer.init()
 pygame.mixer_music.load('ex021.mp3')
 pygame.mixer.music.play()
 input()
 pygame.event.wait()
+
+# Posso usar tambem
+
+import playsound
+
+playsound.playsound('ex021.mp3')

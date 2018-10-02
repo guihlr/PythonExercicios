49d29f2db8d215be48ff0030663cee234e25a714
guihlr <guilhermehlr9@gmail.com>
2018-10-02 17:45:09 -0300

    inital commit

--- ex28-Jogo da Advinhação v1.0-If.else.py
+++ ex28-Jogo da Advinhação v1.0-If.else.py
@@ -3,11 +3,12 @@
 
 from random import randint
 from time import sleep
-computador = randint(0, 5) # Faz o computador "PENSAR"
-print('-=-'*20)
+
+computador = randint(0, 5)  # Faz o computador "PENSAR"
+print('-=-' * 20)
 print('Vou pensar em um número entre 0 e 5. Tente adivinhar ... ')
-print('-=-'*20)
-jogador = int(input('Em que número eu pensei? ')) # Jogador tenta advinhar
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei? '))  # Jogador tenta advinhar
 print('PROCECESSANDO...')
 sleep(2)
 if jogador == computador:

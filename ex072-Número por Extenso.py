bb0d67bf4d7aaf2606f5ab898bfc609d99cfd9a8
guihlr <guilhermehlr9@gmail.com>
2019-01-14 11:43:25 -0200

    estudando exercício

--- ex072-Número por Extenso.py
+++ ex072-Número por Extenso.py
@@ -1,4 +1,4 @@
-# Exercício Python 072: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso,
+# Exercício Python 072: Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso,
 # de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
 cont = ('zero', 'um', 'dois', 'três', 'quatro',
@@ -7,9 +7,40 @@ cont = ('zero', 'um', 'dois', 'três', 'quatro',
         'quinze', 'dezesseis', 'dezessete', 'dezoito',
         'dezenove', 'vinte')
 
+""" Meus Programas """
+"""n = int(input('Digite um número entre 0 e 20: '))
+while n > 20 or n < 0:
+    print('Tente novamente. ', end='')
+    n = int(input('Digite um número entre 0 e 20: '))
+print(f'Você digitou o número {cont[n]}')
+
 while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+    if n < 0 or n > 20:
+        print('Tente novamente. ', end='')
+    else:
+        print(f'Você digitou o número {cont[n]}')
+        break"""
+
+""" Do Guanabara """
+"""while True:
     num = int(input('Digite um número entre 0 e 20: '))
     if 0 <= num <= 20:
         break
     print('Tente novamente. ', end='')
-print(f'Você digitou o número {cont[num]}')
+    print(f'Você digitou o número {cont[num]}')"""
+
+while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= n <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Você digitou o número {cont[n]}')
+for p in cont:
+    p = input('Deseja continuar? [S/N] ')
+    if p.upper() == 'S':
+        n = int(input('Digite outro número entre 0 e 20: '))
+        print(f'Você digitou o número {cont[n]}')
+    else:
+        break
+

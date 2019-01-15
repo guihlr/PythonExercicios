b94a958961343ba905eb2a62ccc16880c06bb0e6
guihlr <guilhermehlr9@gmail.com>
2019-01-15 14:53:07 -0200

    estudando exercício

--- ex076-Lista de Preços com Tupla.py
+++ ex076-Lista de Preços com Tupla.py
@@ -16,6 +16,5 @@ print('-' * 40)
 for pos in range(0, len(listagem)):
     if pos % 2 == 0:
         print(f'{listagem[pos]:.<30}', end='')
-    if pos % 2 == 1:
-        print(f'R$ {listagem[pos]:>5.2f}')
-print('-' * 40)
+    else:
+        print(f'R${listagem[pos]:>7.2f}')

8ba3b1661a83f0c53f29b8abcc334cf45c16ddd5
guihlr <guilhermehlr9@gmail.com>
2019-01-18 19:21:38 -0200

    estudando exercício

--- teste084.py
+++ teste084.py
@@ -3,7 +3,6 @@
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
 
-""" Meu Programa """
 temp = []
 princ = []
 maior = menor = 0
@@ -28,36 +27,3 @@ print(f'\nO menor peso foi de {menor:.1f}Kg. Peso de ', end='')
 for p in princ:
     if p[1] == menor:
         print(f'[{p[0]}]', end='')
-
-""" Do Guanabara """
-
-temp = []
-princ = []
-mai = men = 0
-
-while True:
-    temp.append(str(input('Digite o seu nome: ')))
-    temp.append(float(input('Peso: ')))
-    princ.append(temp[:])
-    if len(temp) == 0:
-        mai = men = temp[1]
-    else:
-        if temp[1] > mai:
-            mai = temp[1]
-        else:
-            men = temp[1]
-    temp.clear()
-    resp = str(input('Deseja continuar? [S/N] '))
-    if resp in 'nN':
-        break
-print('-=' * 30)
-print(f'Os dados foram {princ}')
-print(f'Ao todo, foram {len(princ)} pessoas cadastradas.')
-print(f'O maior peso foi de {mai}Kg. Peso de ', end='')
-for p in princ:
-    if p[1] == mai:
-        print(f'[{p[0]}] ', end='')
-print(f'\nO menor peso foi de {men}Kg. Peso de ', end='')
-for p in princ:
-    if p[1] == men:
-        print(f'[{p[0]}] ', end='')

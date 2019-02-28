7a44b399203e8a5819ae508224c5634766864093
guihlr <guilhermehlr9@gmail.com>
2019-02-28 17:00:52 -0300

    att

--- ex003-Somando números.py
+++ ex003-Somando números.py
@@ -1,5 +1,6 @@
 # Crie um programa que leia dois numeros e mostre a soma entre eles.
-n1 = int(input('Digite um nero: '))
-n2 = int(input('Digite outro nero: '))
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
 soma = n1 + n2
-print('A soma entre eles é de = {}' .format(soma))
+print('A soma entre eles é = {}' .format(soma))

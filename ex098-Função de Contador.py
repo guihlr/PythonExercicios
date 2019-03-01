a82af15dfb5bf9f05e33fd16cb480cac0ebcfc28
guihlr <guilhermehlr9@gmail.com>
2019-03-01 19:43:15 -0300

    exercicio de fixação

--- ex098-Função de Contador.py
+++ ex098-Função de Contador.py
@@ -8,35 +8,29 @@
 from time import sleep
 
 def contador(i, f, p):
+  print(f"Contagem de {p} até {f} de {p} em {p}")
   if p < 0:
     p *= - 1
   if p == 0:
     p = 1
-  print('-=' * 30)
-  print(f'Contagem de {i} até {f} de {p} em {p}')
-  sleep(2)  
-
   if i < f:
     cont = i
     while cont <= f:
-      print(f'{cont}', end=', ', flush=True)
-      sleep(0.5)
+      print(cont)
       cont += p
-    print('FIM!')
-  else:
-    cont = i
-    while cont >= f:
-      print(f'{cont}', end=', ', flush=True)
       sleep(0.5)
-      cont -= p
-    print('FIM!')
+  else:
+    if i > f:
+      cont = i
+      while cont >= f:
+        print(cont)
+        cont -= p
+        sleep(0.5)
 
 # Programa Principal
 contador(1, 10, 1)
-contador(10, 0 , 2)
-print('-=' * 30)
-print('Agora é sua vez de personalizar a contagem!')
-ini = int(input('Inicio: '))
-fim = int(input('Fim: '))
-pas = int(input('Passo: '))
+print("Agora é sua vez de personalizar a contagem!")
+ini = int(input("Início: "))
+fim = int(input("Fim: "))
+pas = int(input("Passo: "))
 contador(ini, fim, pas)

96c00a66f167956815ac1f4e1e60874f24c40818
guihlr <guilhermehlr9@gmail.com>
2019-03-01 22:03:49 -0300

    exercício de fixação

--- ex096-Função que calcula área.py
+++ ex096-Função que calcula área.py
@@ -1,15 +1,11 @@
 # Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno
 # retangular (largura e comprimento) e mostre a área do terreno.
 
-
-def area(larg, comp):
-    a = larg * comp
-    print(f'A área de um terreno {larg} x {comp} é de {a}m²')
-
+def área(l, c):
+  soma = l * c
+  print(f"A área de um terreno de {l}x{c} é de {soma}m²")
 
 # Programa Principal
-print('Controle de Terrenos')
-print('-' * 20)
-l = float(input('LARGURA (m): '))
-c = float(input('COMPRIMENTO (m): '))
-area(l, c)
+l = float(input("LARGURA (m): "))
+c = float(input("COMPRIMENTO (m): "))
+área(l, c)

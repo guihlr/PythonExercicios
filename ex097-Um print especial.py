7a3bc795be68251a05e108214d35ea21c9dcce1e
guihlr <guilhermehlr9@gmail.com>
2019-03-01 21:52:55 -0300

    exercício de fixação

--- ex097-Um print especial.py
+++ ex097-Um print especial.py
@@ -2,18 +2,17 @@
 # como parâmetro e mostra uma mensagem com tamanho adaptável.
 
 def escreva(msg):
-  print('~' * len(msg))
+  print(len(msg) * "~")
   print(msg)
-  print('~' * len(msg))
-  # var = '~'
-  # for p in msg:
-  #   var += '~'
-  # print(var)
-  # print(msg)
-  # print(var)
+  print(len(msg) * "~")
+  """var = "~"
+  for p in msg:
+    var += "~"
+  print(var)
+  print(msg)
+  print(var)"""
 
-
-#Programa Principal
-escreva('Guilherme Henrique')
-escreva('Curso de Python no Youtoba')
-escreva('CeV')
+# Programa Principal
+escreva("Texto")
+escreva("Meu nome é: Guilherme Henrique")
+escreva("Exercício de Fixação!")
